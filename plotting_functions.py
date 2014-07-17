ba0796c420c6b89a3ca449e249e637c31622161b
David Zwicker <zwicker@pks.mpg.de>
2014-07-17 17:01:09 -0400

    Added the class MinorSymLogLocator which is useful to locate minor ticks on plots that have been scaled with the `symlog` scale. I rediscovered this class, when I asked google for a solution to this problem. It turned out someone on stackoverflow had the same problem and a solution was posted. After trying to upvote, I found out that it was actually me that posted both the question and the answer half a year ago. Thank you stackoverflow for preserving my own thoughts!

--- plotting_functions.py
+++ plotting_functions.py
@@ -10,6 +10,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.colors as mclr
 from matplotlib.patches import Polygon
+from matplotlib.ticker import Locator
 
 from figure_presets import set_presentation_style_of_axis
 
@@ -474,6 +475,46 @@ def get_hatched_image(values, stripe_width=0.05, orientation='/'):
             res[x, y] = np.nonzero(values[x, y, :])[0][i]
 
     return res
+
+
+
+class MinorSymLogLocator(Locator):
+    """
+    Dynamically find minor tick positions based on the positions of
+    major ticks for a symlog scaling.
+    """
+    def __init__(self, linthresh):
+        """
+        Ticks will be placed between the major ticks.
+        The placement is linear for x between -linthresh and linthresh,
+        otherwise its logarithmically
+        """
+        self.linthresh = linthresh
+
+    def __call__(self):
+        'Return the locations of the ticks'
+        majorlocs = self.axis.get_majorticklocs()
+
+        # iterate through minor locs
+        minorlocs = []
+
+        # handle the lowest part
+        for i in range(1, len(majorlocs)):
+            majorstep = majorlocs[i] - majorlocs[i-1]
+            if abs(majorlocs[i-1] + majorstep/2) < self.linthresh:
+                ndivs = 10
+            else:
+                ndivs = 9
+            minorstep = majorstep / ndivs
+            locs = np.arange(majorlocs[i-1], majorlocs[i], minorstep)[1:]
+            minorlocs.extend(locs)
+
+        return self.raise_if_exceeds(np.array(minorlocs))
+
+    def tick_values(self, vmin, vmax):
+        raise NotImplementedError('Cannot get tick locations for a '
+                                  '%s type.' % type(self))
+
 
 
 if __name__ == "__main__":
